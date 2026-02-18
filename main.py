befc668628fefea49f96b309ac1a1eb84b403edc
pshoxsanam2197 <pshoxsanam2197@gmail.com>
2026-02-18 17:00:25 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,71 @@
+# # 1-masala
+# numbers = (15, 8, 22, 3, 19)
+# print(max(numbers))
+
+# # 2-masala
+# numbers = (4, 9, 16, 25, 36)
+# if 16 in numbers:
+#     print("16 mavjud")
+# else:
+#     print("16 mavjud emas")
+
+# # 3-masala
+# numbers = (1, 2, 3, 4, 5)
+# numbers = list(numbers)
+# print(numbers[::-1])
+
+# # 4-masala
+# numbers = (12, 7, 18, 5, 20)
+#
+# t = sum(1 for num in numbers if num % 2 == 0)
+#
+# print(t)
+
+# # 5-masala
+# t1 = (1, 2, 3)
+#
+# t2 = (4, 5, 6)
+#
+# t3 = t1 + t2
+#
+# print(t3)
+
+# # 6-masala
+# data = ("Ali", 25, "Toshkent", "Dasturchi")
+#
+# print(data)
+#
+# print(data[2])
+
+
+# # 7-masala
+# words = ("python", "code", "AI", "developer")
+#
+# for word in words:
+#     print(len(word))
+#
+#
+# # 8-masala
+# numbers = (7, 8)
+#
+# print(numbers)
+#
+# for i in range(4):
+#     print(numbers)
+#
+#
+# # 9-masala
+# numbers = (45, 12, 78, 5, 34)
+#
+# print(numbers)
+#
+# print(min(numbers))
+#
+#
+# # 10-masala
+# numbers = (3, 6, 9, 12)
+#
+# print(numbers)
+#
+# summa = sum(numbers)
+# print(summa)
